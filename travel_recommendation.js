ea543749dd5fa05f81f98fe03cea56b6a5f20aa9
noxbike <noxbike@gmail.com>
2024-08-18 20:55:56 +0400

    make content responsive

---
+++ travel_recommendation.js
@@ -0,0 +1,40 @@
+var searchButton = document.getElementById("search");
+var clear = document.getElementById("clear");
+var searchText = document.getElementById("input-search");
+var listResult = document.getElementById("list-result");
+
+searchButton.addEventListener("click", () =>{
+    let text = searchText.value
+
+    getData(text.toLowerCase());
+    
+});
+clear.addEventListener("click", () =>{
+    searchText.value = "";
+});
+
+const getData = (param) => {
+    fetch("/travel_recommendation_api.json")
+        .then(response => response.json())
+        .then(data => {
+            let array = data[param];
+            listResult.innerHTML = "<li class='top-bar'></li>";
+            array.forEach(element => {
+                console.log(element)
+                if(param === "countries"){
+                    element["cities"].forEach(country => {
+                        createListOfRecommendation(country)
+                    })
+                }
+                else{
+                   createListOfRecommendation(element)
+                }   
+                })   
+            });
+}
+
+const createListOfRecommendation = (element) => {
+    let li = document.createElement("li");
+    li.innerHTML = `<div class="background-city" style="background-image:url(${element.imageUrl})"></div><h4>${element.name}</h4><p>${element.description}</p><button>Visit</button>`
+    listResult.appendChild(li)
+}
